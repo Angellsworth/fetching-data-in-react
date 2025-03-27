fcde4fd32876caa0619afb13c7988793f76b1262
Angellsworth <angellsworth@msn.com>
2025-03-27 15:18:34 -0600

    wip: in the middle of the lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import { useState } from 'react'
+// the star means import all
+import * as weatherService from './services/weatherService';
+import WeatherSearch from './components/WeatherSearch/WeatherSearch';
+
 import './App.css'
 
 const App = () => {
+  const [weater, setWeather] = useState({});
+  
+  const fetchData = async () => {
+    const data = await weatherService.show('Munchen')
+    const newWeatherState = {
+      location: data.location.name,
+      temperature: data.current.temp_f,
+      condition: data.current.condition.text,
+    };
+    setWeather(newWeatherState)
+  }
 
   return (
-    <h1>Hello world!</h1>
+    <main>
+      <h1>Weather API</h1>
+      <WeatherSearch />
+    </main>
   );
 }
 
